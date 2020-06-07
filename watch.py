b33effdda83008d425a0344833b9e735e00325c4
Alexis Robles <alexis.robles49@gmail.com>
2020-06-07 19:32:17 -0400

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,5 +1,8 @@
 import math
 import time
+from datetime import datetime as dt
+from datetime import time as tim
+from datetime import timedelta as td
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -33,6 +36,26 @@ def secToTime(sec):
 
     return hr + ':' + min + ':' + sec
 
+def findReplayXPath():
+    first = '//*[@id="movie_player"]/div['
+    second = ']/div[2]/div[1]/button'
+    for num in range(24, 26):
+        try:
+            replayButton = browser.find_element_by_xpath(first + str(num) + second).get_attribute("title")
+            return replayButton
+        except:
+            pass
+    return ''
+
+def adTime(totalRunTime):
+    videoLen = td(seconds = 3386)
+    secDiff = totalRunTime.seconds - videoLen.seconds
+    
+    if secDiff > 0:
+        return str(td(seconds = secDiff))
+    else:
+        return '(no ads were detected or the video was cut short)'
+
 # Plays the whole video with ads over and over (restarts after end)
 while True:
     broken = False
@@ -62,6 +85,9 @@ while True:
 
     # Play Full Video and then Quit
     if broken == False:
+
+        initialTime = dt.now().replace(microsecond = 0)
+
         time.sleep(5)
 
         # Loop to test if the "Replay" button exists yet (it only does when the video is over)
@@ -79,11 +105,15 @@ while True:
 
             # Try to get 'Replay' element
             try:
-                replayButton = browser.find_element_by_xpath('//*[@id="movie_player"]/div[25]/div[2]/div[1]/button').get_attribute("title")
-                if replayButton == 'Replay':
+                replayButton = findReplayXPath()
+                if replayButton == 'Replay':   
                     print('\nVideo Played in Full')
-                    print('It took {} to play the entire video with ads'.format(secToTime(seconds)))
-                    print('Ad time totaled {}'.format(secToTime(seconds - 3386)))
+
+                    currentTime = dt.now().replace(microsecond = 0)
+                    totalRunTime = (currentTime - initialTime)
+
+                    print('It took {} to play the entire video with ads'.format(totalRunTime))
+                    print('Ad time totaled {}'.format(adTime(totalRunTime)))
                     print('Restarting...\n')
                     browser.quit()
                     break
